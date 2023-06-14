023f018997a7abe1f7614110a49ba246e78251a9
Austin <austinodhiambo64@gmail.com>
2023-06-14 16:40:12 +0300

    Updates

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const element = document.getElementById("myBtn");
 element.addEventListener("click", function() {
-    alert("Glad you hit me. Reach this guy at 0724352623");
+    alert("Glad you hit me. Reach me at 0724352623");
+});
+
+// JavaScript code
+window.addEventListener('DOMContentLoaded', function() {
+    // Hero section animation
+    var heroContent = document.querySelector('.hero-content');
+    heroContent.classList.add('show');
+
+    // Project section animation
+    var projectSection = document.querySelector('.project-section');
+    projectSection.classList.add('show');
+
+    // Contact section animation
+    var contactSection = document.querySelector('.contact-section');
+    contactSection.classList.add('show');
 });
